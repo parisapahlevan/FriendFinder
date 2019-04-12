5bc64a2e4fe6019a96343803dffd5e03209c1695
Parisa Pahlevan <parisapahlevan@Zouhairs-MacBook-Pro-2.local>
2019-04-12 18:13:12 -0400

    AddedDesignAndFunctionality

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+        name: "Tala",
+        img: "../public/images/celine-sayuri-tagami-448870-unsplash.jpg",
+        scores: [1,2,3,4,5,1,2,3,4,5]
+    },
+    {
+        name: "Riley",
+        img: "../public/images/alexandru-rotariu-251789-unsplash.jpg",
+        scores: [2,3,5,4,1,4,5,2,3,1]
+    },
+    {
+        name: "Stella",
+        img: "../public/images/baptist-standaert-346832-unsplash.jpg",
+        scores: [1,5,4,2,3,4,4,3,1,2]
+    },
+    {
+        name: "Prince",
+        img: "../public/images/charles-464973-unsplash.jpg",
+        scores: [3,5,2,1,5,3,1,3,2,5]
+    },
+    {
+        name: "Freddie",
+        img: "../public/images/jen-b-1053443-unsplash.jpg",
+        scores: [4,5,2,2,1,5,3,2,1,5]
+    },
+    {
+        name: "Lulu",
+        img: "../public/images/kieran-white-235113-unsplash.jpg",
+        scores: [3,5,2,2,1,4,3,5,2,1]
+    },
+    {
+        name: "Moon",
+        img: "../public/images/charles-550068-unsplash.jpg",
+        scores: [2,3,1,1,4,3,5,2,1,1]
+    },
+    {
+        name: "Mahi",
+        img: "../public/images/matthew-miles-1232263-unsplash.jpg",
+        scores: [5,4,2,1,4,4,5,3,2,1]
+    },
+    {
+        name: "Boo",
+        img: "../public/images/tobias-a-muller-771985-unsplash.jpg",
+        scores: [4,5,3,2,2,2,3,1,1,1]
+    },
+    {
+        name: "Cool",
+        img: "../public/images/the-poodle-gang-369918-unsplash",
+        scores: [3,4,2,1,4,2,5,3,2,1]
+    }
+]
+module.exports = friends;
