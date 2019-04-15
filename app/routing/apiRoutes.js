342cf7d7cf7c32a538949b8555de233bb6240d3a
Parisa Pahlevan <parisapahlevan@Zouhairs-MacBook-Pro-2.local>
2019-04-14 22:01:05 -0400

    finishe my findfriend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,51 @@
-var path = require("path");
-var friends = require("../data/friends");
+var friends = require("../data/friends.json");
+const fs = require('fs');
+
+function findTheIndexOfSmallest(arr) {
+    var index = 0;
+    var smallest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < smallest ) {
+            smallest  = arr[i]
+            index = i;
+        }
+    }
+    return arr.indexOf(smallest);
+}
+
+function findMatch(data, userData){
+    var result = [];
+    for(let i=0; i<data.length; i++){
+        var sum = 0;
+        for (let j=0; j<data[i].scores.length; j++){
+            var diff = Math.abs(data[i].scores[j] - userData.scores[j])
+            sum += diff 
+        }
+        result.push(sum);
+    }
+    var matchIndex = findTheIndexOfSmallest(result);
+    return data[matchIndex];
+}
+
+
 
 module.exports = function (app){
+
     app.get("/api/friends", function(req,res){
-        res.json(friends)
-    })
-    app.post("/api/friends", function(req,res){
-        console.log(req)
-        res.json(friends)
-    })
-    
+        res.json(friends);
+    }); 
+
+    app.post("/api/survey", function(req,res){
+        var newRecord = {
+            name: req.body.name,
+            img: req.body.photo,
+            scores: req.body['scores[]'].map(elem => parseInt(elem))
+        }
+        var match = findMatch(friends, newRecord)
+        friends.push(newRecord)
+        fs.writeFile('./app/data/friends.json', JSON.stringify(friends) , function (err) {
+            if (err) throw err;
+            res.json(match);
+          });
+    }); 
 }
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require("express");
 var bodyParser = require("body-parser");
+var express = require("express");
+var path = require("path");
 
 var app = express();
-
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(bodyParser.json());
-
+app.use(bodyParser.json())
+ 
 app.use(express.static('./app/public'))
-
-require("./app/routing/apiRoutes")(app)
-require("./app/routing/htmlRoutes")(app)
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function(req, res){
-    console.log("app listening on port" + PORT)
+    console.log("App listening on PORT" + PORT)
 });
+    
